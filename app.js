52b21944673312fa725771d1400b284d72dcf23d
Anton Rumyantsev <blush3d@ya.ru>
2023-07-30 00:20:56 +0300

    test

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.post('/signin', celebrate(
     body: {
       email: Joi.string().required().email(),
       password: Joi.string().required(),
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
       avatar: Joi.string().required().regex(regex),
     },
   },
@@ -42,8 +42,8 @@ app.post('/signup', celebrate(
     body: {
       email: Joi.string().required().email(),
       password: Joi.string().required(),
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
       avatar: Joi.string().required().regex(regex),
     },
   },
