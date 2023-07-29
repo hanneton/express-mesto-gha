43b8ac5418636da1c6335078099bdfe604d43bb1
Anton Rumyantsev <blush3d@ya.ru>
2023-07-30 01:09:53 +0300

    test

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/signup', celebrate(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().required().regex(regex),
+      avatar: Joi.string().regex(regex),
     },
   },
 ), createUser);
