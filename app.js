25cfacdf6e7fe4781a706776b57ed30e9c7be995
Anton Rumyantsev <blush3d@ya.ru>
2023-07-29 23:27:14 +0300

    test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signin', celebrate(
   {
     body: {
       email: Joi.string().required().email(),
-      password: Joi.string().required().regex(),
+      password: Joi.string().required(),
       avatar: Joi.string().required().regex(regex),
       name: Joi.string().required().min(2).max(30),
       about: Joi.string().required().min(2).max(30),
